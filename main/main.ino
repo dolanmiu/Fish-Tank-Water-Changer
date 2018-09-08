0f7d59c13670cbc8a1857d9670508a3d89857bc2
Dolan Miu <dolan_miu@hotmail.com>
2018-09-08 23:26:25 +0100

    Set 5 minute each way per day

--- main/main.ino
+++ main/main.ino
@@ -3,6 +3,8 @@ int M1 = 12;
 int E2 = 11;
 int M2 = 13;
 
+int durationInSeconds = 300;
+
 void setup()
 {
   pinMode(M1, OUTPUT);
@@ -11,28 +13,24 @@ void setup()
 
 void loop()
 {
-  { 
-    int value;
-    for (value = 0 ; value <= 255; value += 5)
-    {
-      digitalWrite(M1, HIGH);
-      digitalWrite(M2, HIGH);
-      analogWrite(E1, value);
-      analogWrite(E2, value);
-      delay(30);
-    }
-    delay(1000);
-  }
-  { 
-    int value;
-    for (value = 0 ; value <= 255; value += 5)
-    {
-      digitalWrite(M1, LOW);
-      digitalWrite(M2, LOW);
-      analogWrite(E1, value);
-      analogWrite(E2, value);
-      delay(30);
-    }
-    delay(1000);
-  }
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
+  analogWrite(E1, 255);
+  analogWrite(E2, 255);
+
+  delay(durationInSeconds * 1000);
+
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, LOW);
+  analogWrite(E1, 255);
+  analogWrite(E2, 255);
+
+  delay(durationInSeconds * 1000);
+
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
+
+  // Wait one day
+  delay(86400 * 1000);
+  
 }
